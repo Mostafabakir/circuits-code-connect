6ca3e471ce5584cb6a6107e805367a5dddee0265
mostafa <mostafabakir234@gmail.com>
2025-04-20 01:08:32 +0200

    V1.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/circuits-code-connect/', // Add your repository name here
   server: {
     host: "::",
     port: 8080,
